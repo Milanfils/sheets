33abeb0893c286f2face9593bf38a5980952f824
Gavin D'souza <gavin18d@gmail.com>
2023-06-24 14:03:18 +0530

    test: Add test for safe monkey patch

--- sheets/google_workspace/doctype/google_spreadsheet/google_spreadsheet.py
+++ sheets/google_workspace/doctype/google_spreadsheet/google_spreadsheet.py
@@ -356,5 +356,7 @@ def patch_importer():
 
     _official_method = Importer.update_record
     Importer.update_record = update_record_patch
+    Importer.patched = True
     yield
     Importer.update_record = _official_method
+    del Importer.patched
